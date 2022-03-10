c06873a59dda57150495a11ecec8b41f4dfbd564
FilipToth <filip.toth@accelley.com>
2022-03-10 16:19:04 +0100

    Add basic 3n+1 solver

--- main.cu
+++ main.cu
@@ -1,44 +1,60 @@
 #include <stdio.h>
+#include <math.h>
 
-__global__ void kernel(int *num1, int *num2, int *result) {
-    *result = *num1 + *num2;
+__global__ void kernel(double *n, bool *result, double *count) {
+    *result = true;
+
+    while (*n != 1)
+    {
+        // perform modulo
+        // a - (a / b) * b => a % b
+        if ((*n - (*n / 2) * 2) == 0)
+        {
+            *n = *n / 2;
+        }
+        else
+        {
+            *n = *n * 3;
+            *n = *n + 1;
+        }
+
+        *count = *count + 1;
+    }
 }
-
-// 3n+1 problem:
-// 
-//
-//
 
 int main() {
     // Host copies
-    int result, num1, num2;
+    double n;
+    bool result;
+    double count;
 
     // CUDA Device copies
-    int *result_ptr, *num1_ptr, *num2_ptr;
+    double *n_ptr;
+    bool *result_ptr;
+    double *count_ptr;
     
-    cudaMalloc(&num1_ptr, sizeof(int));
-    cudaMalloc(&num2_ptr, sizeof(int));
-    cudaMalloc(&result_ptr, sizeof(int));
+    cudaMalloc(&n_ptr, sizeof(double));
+    cudaMalloc(&result_ptr, sizeof(bool));
+    cudaMalloc(&count_ptr, sizeof(double));
 
     // nums to add
-    num1 = 2;
-    num2 = 6;
+    n = 4;
 
     // copy to device
-    cudaMemcpy(num1_ptr, &num1, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(num2_ptr, &num2, sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(n_ptr, &n, sizeof(double1), cudaMemcpyHostToDevice);
     
     // start
-    kernel<<<1, 1>>>(num1_ptr, num2_ptr, result_ptr);
+    kernel<<<1, 1>>>(n_ptr, result_ptr, count_ptr);
 
     // copy result from device
-    cudaMemcpy(&result, result_ptr, sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&result, result_ptr, sizeof(bool), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&count, count_ptr, sizeof(double), cudaMemcpyDeviceToHost);
 
-    printf("%d\n", result);
+    printf("%lf: %d | with %lf operations\n", n, result, count);
 
     cudaDeviceSynchronize();
     cudaError_t error = cudaGetLastError();
-    if(error!=cudaSuccess)
+    if(error != cudaSuccess)
     {
         fprintf(stderr,"ERROR: %s\n", cudaGetErrorString(error) );
         exit(-1);
